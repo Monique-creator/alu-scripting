4138e4a5fd1f3db39bb36a56dac1f2631195cd28
Monique-creator <m.niyobyose@alustudent.com>
2026-02-22 18:37:15 +0000

    api project

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to query Reddit API for subscriber count."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "python:subreddit.sub.count:v1.0 (by /u/yourusername)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    return 0
